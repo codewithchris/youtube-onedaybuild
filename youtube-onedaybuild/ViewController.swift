50ebcc398fa27e2a8b23ad1fbbf90176d0e82c16
codewithchris <chris@codewithchris.com>
2020-05-18 18:01:37 -0400

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Christopher Ching on 2020-05-18.
+//  Copyright © 2020 Christopher Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
