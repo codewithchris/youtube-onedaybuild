7229140b9ab5dd95c03a50c9f9e615bcc481bb9d
codewithchris <chris@codewithchris.com>
2020-05-29 16:47:50 -0400

    removed api key

--- youtube-onedaybuild/Constants.swift
+++ youtube-onedaybuild/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = "AIzaSyCAFyChagdXsVBTwuRN5lRmgI0z4VXb5zM"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
